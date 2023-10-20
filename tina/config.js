a125750b6c570fc53bcc6b287b7ce7a1e529db05
ImNotAOwl <siegel.matthieu@ymail.com>
2023-10-20 22:33:52 +0200

    typo

--- tina/config.js
+++ tina/config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "tinacms";
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch =
+  process.env.NEXT_PUBLIC_TINA_BRANCH ||
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
 
 export default defineConfig({
   branch,
