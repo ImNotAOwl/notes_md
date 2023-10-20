b9c9ab7af1e93e2efb7f453b77a6e3ba2f174663
ImNotAOwl <siegel.matthieu@ymail.com>
2023-10-20 22:23:28 +0200

    add env

--- tina/config.js
+++ tina/config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "tinacms";
-
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+  clientId: process.env.TINA_CLIENTID, // Get this from tina.io
+  token: process.env.TINA_TOKEN, // Get this from tina.io
 
   build: {
     outputFolder: "admin",
